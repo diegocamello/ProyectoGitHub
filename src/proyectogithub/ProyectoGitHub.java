594ed4c20226671bacc446c559d408aa2a163613
Dgocampu <38407836+Dgocamp@users.noreply.github.com>
2018-06-25 22:01:34 -0400

    segundo aporte

--- src/proyectogithub/ProyectoGitHub.java
+++ src/proyectogithub/ProyectoGitHub.java
@@ -17,6 +17,7 @@ public class ProyectoGitHub {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Este es un proyecto de github");
+        System.out.println("Este es un cambio");
     }
     
 }
